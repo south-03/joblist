a78f1280934fc2dd9a3390f01c51c8475946f52c
south-03 <lehoduclam@gmail.com>
2023-04-04 20:04:06 +0700

    SearchBar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
-import { Box, ThemeProvider } from "@material-ui/core";
+import { Box, ThemeProvider, Grid } from "@material-ui/core";
 import theme from "./theme/theme";
 import Header from "./component/Header";
+import SearchBar from "./component/SearchBar";
 
 export default () => {
   return (
     <ThemeProvider theme={theme}>
       <Header />
+      <Grid container justify="center">
+        <Grid item xs={10}>
+          <SearchBar />
+        </Grid>
+      </Grid>
     </ThemeProvider>
   );
 };
